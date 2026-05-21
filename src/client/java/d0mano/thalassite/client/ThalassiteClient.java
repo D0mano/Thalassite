77a8d05a9634c914236e797044089a999689b2e8
Nolann Fotso <njfotso@gmail.com>
2026-05-21 21:14:27 +0200

    Initial commit

---
+++ src/client/java/d0mano/thalassite/client/ThalassiteClient.java
@@ -0,0 +1,10 @@
+package d0mano.thalassite.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThalassiteClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
